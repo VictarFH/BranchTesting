7445daf1d0758b65d442a3a295b4123221443bf4
VictarFH <vihe0001.stud.kea.dk>
2023-11-13 09:12:44 +0100

    makeSound method

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,5 +1,5 @@
 public abstract class Animal {
     private String name;
 
-
+    public abstract void makeSound();
 }
--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,4 +1,7 @@
 public class Dog extends Animal {
 
+    @Override
+    public void makeSound() {
 
+    }
 }
